d1164fdd0704b513705f299142c15b795d2d8c3e
morrs <grishkovea@gmail.com>
2026-03-25 18:48:22 +0300

    feat: добавил базовую сущность и vo

---
+++ user_service/src/main/java/org/example/user_service/domain/base/BaseEntity.java
@@ -0,0 +1,11 @@
+package org.example.user_service.domain.base;
+
+import org.example.user_service.domain.base.exceptions.ValidateException;
+
+import java.util.UUID;
+
+public abstract class BaseEntity {
+
+    private UUID id;
+
+}
