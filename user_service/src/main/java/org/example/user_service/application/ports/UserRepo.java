7bd1ec7bc09541c8c2cb6053b7152f0387af3beb
morrs <grishkovea@gmail.com>
2026-04-03 11:50:36 +0300

    feat: добавил репу и сущщность пользователя для hibernate

---
+++ user_service/src/main/java/org/example/user_service/application/ports/UserRepo.java
@@ -0,0 +1,7 @@
+package org.example.user_service.application.ports;
+
+import java.util.UUID;
+
+public interface UserRepo {
+    UUID createUser();
+}
---
+++ user_service/src/main/java/org/example/user_service/infrastructure/persistence/models/User.java
@@ -0,0 +1,9 @@
+package org.example.user_service.infrastructure.persistence.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+}
