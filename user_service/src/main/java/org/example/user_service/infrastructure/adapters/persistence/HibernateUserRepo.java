d7fc32de269ce41efdc5737dd25df8ba0712948b
morrs <grishkovea@gmail.com>
2026-04-06 10:52:24 +0300

    feat: добавил логику работы с бд юзера через hibernate

--- user_service/src/main/java/org/example/user_service/infrastructure/adapters/persistence/HibernateUserRepo.java
+++ user_service/src/main/java/org/example/user_service/infrastructure/adapters/persistence/HibernateUserRepo.java
@@ -4,9 +4,12 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.example.user_service.application.interactors.create_user.CreateUserCommand;
 import org.example.user_service.application.ports.UserRepo;
+import org.example.user_service.infrastructure.persistence.models.User;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class HibernateUserRepo implements UserRepo {
@@ -16,8 +19,20 @@ public class HibernateUserRepo implements UserRepo {
 
     @Override
     public UUID createUser(CreateUserCommand userCommand) {
-
-
-        return null;
+        var newUser = new User(
+                UUID.randomUUID(),
+                userCommand.surname(),
+                userCommand.name(),
+                userCommand.patronymic(),
+                userCommand.userStatus(),
+                userCommand.userEmail(),
+                userCommand.userPassword(),
+                userCommand.userProfilePhotoLink(),
+                userCommand.currentCourses(),
+                userCommand.finishedCourses(),
+                userCommand.certificates()
+        );
+        entityManager.merge(newUser);
+        return newUser.getId();
     }
 }
