293eb5744b3e6ae1af0fb835fa7aca724b9b2a7f
morrs <grishkovea@gmail.com>
2026-04-07 17:50:46 +0300

    feat: добавил маппер для пользователя

--- user_service/src/main/java/org/example/user_service/presentation/http/v1/user/create/handlers/CreateHandler.java
+++ user_service/src/main/java/org/example/user_service/presentation/http/v1/user/create/handlers/CreateHandler.java
@@ -1,11 +1,14 @@
 package org.example.user_service.presentation.http.v1.user.create.handlers;
 
 import lombok.RequiredArgsConstructor;
-import org.example.user_service.application.interactors.create_user.CreateUserCommand;
 import org.example.user_service.application.interactors.create_user.CreateUserInteractor;
 import org.example.user_service.presentation.http.v1.user.create.dto.CreateUserRequest;
 import org.example.user_service.presentation.http.v1.user.create.dto.CreateUserResponse;
-import org.springframework.web.bind.annotation.*;
+import org.example.user_service.presentation.http.v1.user.mappers.UserMapper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
@@ -13,24 +16,11 @@ import org.springframework.web.bind.annotation.*;
 public class CreateHandler {
 
     private final CreateUserInteractor createUserInteractor;
+    private final UserMapper mapper;
 
     @PostMapping()
     public CreateUserResponse create(@RequestBody CreateUserRequest userRequest) {
-        return new CreateUserResponse(createUserInteractor.create(toCommand(userRequest)));
+        return new CreateUserResponse(createUserInteractor.create(mapper.toCreateUserCommand(userRequest)));
     }
 
-    private CreateUserCommand toCommand(CreateUserRequest userRequest) {
-        return new CreateUserCommand(
-                userRequest.surname(),
-                userRequest.name(),
-                userRequest.patronymic(),
-                userRequest.userStatus(),
-                userRequest.userEmail(),
-                userRequest.userPassword(),
-                userRequest.userProfilePhotoLink(),
-                userRequest.currentCourses(),
-                userRequest.finishedCourses(),
-                userRequest.certificates()
-        );
-    }
 }
---
+++ user_service/src/main/java/org/example/user_service/presentation/http/v1/user/mappers/UserMapper.java
@@ -0,0 +1,13 @@
+package org.example.user_service.presentation.http.v1.user.mappers;
+
+import org.example.user_service.application.interactors.create_user.CreateUserCommand;
+import org.example.user_service.presentation.http.v1.user.create.dto.CreateUserRequest;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    CreateUserCommand toCreateUserCommand(CreateUserRequest userRequest);
+
+
+}
