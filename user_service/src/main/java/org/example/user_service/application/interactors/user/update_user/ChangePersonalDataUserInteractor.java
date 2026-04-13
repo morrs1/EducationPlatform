cb4be8ac8cd5bc2aed07a71137d292d2dbf62d2b
morrs <grishkovea@gmail.com>
2026-04-13 23:07:19 +0300

    fix: унифицировал имена полей

--- user_service/src/main/java/org/example/user_service/application/interactors/user/update_user/ChangePersonalDataUserInteractor.java
+++ user_service/src/main/java/org/example/user_service/application/interactors/user/update_user/ChangePersonalDataUserInteractor.java
@@ -9,40 +9,40 @@ import org.example.user_service.domain.user.services.UserDomainService;
 @RequiredArgsConstructor
 public class ChangePersonalDataUserInteractor {
 
-    private final UserRepo repo;
+    private final UserRepo userRepo;
     private final TransactionManager transactionManager;
     private final UserDomainService domainService;
 
     public void updateName(ChangeUserNameCommand changeUserNameCommand) {
         transactionManager.inTransaction(() -> {
-            var user = repo.readById(changeUserNameCommand.id()).orElseThrow(() -> new UserNotFoundException("User was not found"));
+            var user = userRepo.readById(changeUserNameCommand.id()).orElseThrow(() -> new UserNotFoundException("User was not found"));
             domainService.updateName(user, changeUserNameCommand.newName());
-            repo.update(user);
+            userRepo.update(user);
         });
 
     }
 
     public void updateSurname(ChangeUserSurnameCommand changeUserSurnameCommand) {
         transactionManager.inTransaction(() -> {
-            var user = repo.readById(changeUserSurnameCommand.id()).orElseThrow(() -> new UserNotFoundException("User was not found"));
+            var user = userRepo.readById(changeUserSurnameCommand.id()).orElseThrow(() -> new UserNotFoundException("User was not found"));
             domainService.updateSurname(user, changeUserSurnameCommand.newSurname());
-            repo.update(user);
+            userRepo.update(user);
         });
     }
 
     public void updatePatronymic(ChangeUserPatronymicCommand changeUserPatronymicCommand) {
         transactionManager.inTransaction(() -> {
-            var user = repo.readById(changeUserPatronymicCommand.id()).orElseThrow(() -> new UserNotFoundException("User was not found"));
+            var user = userRepo.readById(changeUserPatronymicCommand.id()).orElseThrow(() -> new UserNotFoundException("User was not found"));
             domainService.updatePatronymic(user, changeUserPatronymicCommand.newPatronymic());
-            repo.update(user);
+            userRepo.update(user);
         });
     }
 
     public void updateStatus(ChangeUserStatusCommand changeUserStatusCommand) {
         transactionManager.inTransaction(() -> {
-            var user = repo.readById(changeUserStatusCommand.id()).orElseThrow(() -> new UserNotFoundException("User was not found"));
+            var user = userRepo.readById(changeUserStatusCommand.id()).orElseThrow(() -> new UserNotFoundException("User was not found"));
             domainService.updateStatus(user, changeUserStatusCommand.newStatus());
-            repo.update(user);
+            userRepo.update(user);
         });
     }
 
