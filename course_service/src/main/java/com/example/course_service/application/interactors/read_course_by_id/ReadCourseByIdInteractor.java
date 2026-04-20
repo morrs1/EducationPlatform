e06868f52dd493e73981c4a1dc749f756426a247
morrs <grishkovea@gmail.com>
2026-04-20 17:41:14 +0300

    feat: добавил дто и мапперы для флоу чтения курса по id, также добавил исключение на случай отсутсвия его в бд

---
+++ course_service/src/main/java/com/example/course_service/application/interactors/read_course_by_id/ReadCourseByIdInteractor.java
@@ -0,0 +1,27 @@
+package com.example.course_service.application.interactors.read_course_by_id;
+
+import com.example.course_service.application.exceptions.CourseNotFoundException;
+import com.example.course_service.application.interactors.mappers.CourseViewMapper;
+import com.example.course_service.application.ports.TransactionManager;
+import com.example.course_service.infrasructure.adapters.persistence.HibernateCourseRepo;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class ReadCourseByIdInteractor {
+
+    private final HibernateCourseRepo courseRepo;
+    private final TransactionManager transactionManager;
+    private final CourseViewMapper mapper;
+
+    public ReadCourseByIdView readById(UUID id) {
+        return mapper.toReadCourseByIdView(
+                transactionManager.inTransaction(() -> courseRepo.readById(id)).orElseThrow(
+                        () -> new CourseNotFoundException("Course with this id was not found")
+                )
+        );
+    }
+
+
+}
