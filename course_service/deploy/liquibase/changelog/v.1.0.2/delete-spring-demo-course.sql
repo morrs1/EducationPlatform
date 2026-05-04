4b9be208105b9a231651ca8fb02cb7a1db525819
morrs <grishkovea@gmail.com>
2026-05-04 21:01:26 +0300

    fix: решил проблему с миграциями

--- course_service/deploy/liquibase/changelog/v.1.0.2/delete-spring-demo-course.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
-
---changeset codex:delete-spring-demo-course
-
-DELETE FROM course
-WHERE id = '51652887-b537-468f-91e0-bc4ee1ddb4fd'
-   OR author_id = '4ddf2ae0-3da9-46c0-a088-a28726f2aff6';
